c16345e46af5f93fc94151684cdc33ded0d95ecb
Nicholas Marshall <nmarshall23@gmail.com>
2013-07-31 11:05:24 -0400

    updated to use new Object handlers

--- src/main/scala/ObfuscaterPlugin.scala
+++ src/main/scala/ObfuscaterPlugin.scala
@@ -1,17 +1,13 @@
 package com.gmail.nmarshall23
 
 import sbt._
-import Keys._
-import org.ldg.mcpd.MCPDInheritanceVisitor
-import org.ldg.mcpd.MCPDFileHandler
-import java.io.IOException
-import org.ldg.mcpd.MCPDInheritanceGraph
-import org.ldg.mcpd.MCPDRemapper
-import java.util.ArrayList
-import org.apache.ivy.Ivy
-import org.apache.ivy.core.module.id.ModuleRevisionId
-import org.apache.ivy.core.resolve.ResolveOptions
+import sbt.Keys._
 
+import scala.collection.JavaConverters._
+import sbt.ConfigKey.configurationToKey
+import sbt.Scoped.t2ToTable2
+import sbt.Scoped.t4ToTable4
+import scala.Array.canBuildFrom
 
 
 object ObfuscaterPlugin extends Plugin
@@ -21,7 +17,6 @@ object ObfuscaterPlugin extends Plugin
 
 object MinecraftReobfuscater {
 
-val recalcInheritance = TaskKey[java.io.File]("Obfuscater-recalc")
 val reobfuscate = TaskKey[java.io.File]("Obfuscater-reobfuscate")
 val copyObfuscated = TaskKey[Unit]("Obfuscater-copyToModDir")
 
@@ -46,119 +41,32 @@ lazy val deobfMinecraftJar = SettingKey[File]("Obfuscater-deobf-mcjar", "Path to
 
 
 lazy val retriveMCFromIvy = SettingKey[(File,File)]("Obfuscater-retriveMCFromIvy")
-lazy val retriveMCFromIvySetting = retriveMCFromIvy <<= minecraftLibrary.apply( RetrieveMCFromIvy(_) )
+lazy val retriveMCFromIvySetting = retriveMCFromIvy <<= minecraftLibrary.apply( IvyHandler.RetrieveModule(_) )
 
-
-
-def RetrieveMCFromIvy(config:ModuleID) = {
-  
-  val ivy = Ivy.newInstance()
-  ivy.configureDefault()
-  val mrid = ModuleRevisionId.newInstance(config.organization, config.name, config.revision)
-  var options = new ResolveOptions()
-  options.setOutputReport(false)
-  options.setRefresh(false)
-  options.setCheckIfChanged(false)
-  options.setUseCacheOnly(true)
-  
-  val report = ivy.resolve(mrid, options, false)
-
-  val getArtifact = {aType:String => (report.getArtifactsReports(mrid)).filter( r => r.getType() == aType).map (r => r.getLocalFile() ).headOption }
-  
-  val srgFile = getArtifact("srg")
-  val jarFile = getArtifact("jar")
-  
-  (srgFile.get,jarFile.get)
-  
-//XXX add Error checking
-  
-//  srgFile match {
-//  	case Some(f) => f 
-//  	case None    => new File("") // Need to throw an error and expain that the SRG file wasn't found in the Ivy Repo
-//  }
-  
-}
-
-val recalcInheritanceTask = recalcInheritance <<= (packageBin in Compile,  deobfMinecraftJar , target) map { (bin:File, mcjar,t) =>
-
-  val inheritanceFile:File = t / "my.inh"
-  
-  val infiles = new ArrayList[File]()
-  infiles.add(bin)
-  infiles.add(mcjar)
-  
-  val re = CalcInheritance(inheritanceFile,new ArrayList[File](),infiles,infiles )
-  inheritanceFile
-}
-
-def CalcInheritance(inhFile:File,
-					stored_inheritance:ArrayList[File],
-				    infiles:ArrayList[File],
-				    outfiles:ArrayList[File]):Option[MCPDInheritanceGraph] = {
-  println("Calculating inheritance...")
-  try {
-    
-	  val inheritance = new MCPDInheritanceVisitor(inhFile,stored_inheritance)
-      val inheritanceProcessor = new MCPDFileHandler(inheritance)
-      val failures = inheritanceProcessor.processFiles(infiles, outfiles)
-	  inheritance.done()
-	  
-	  return Some(inheritance.graph)
-  }
-  catch {
-    case e:IOException => //Log failure
-      return None
-  }
-  
-}
-
-
-
-
-val reobfuscateTask = reobfuscate <<= (packageBin in Compile, recalcInheritance, retroGuardConfig, target) map { (bin:File, inh, config, t) =>
-	val obfzip:File = t / (bin.base + "_ob.jar")
+lazy val reobfuscateTask = reobfuscate <<= (packageBin in Compile, deobfMinecraftJar, retroGuardConfig, target) map {
+    (bin:File, mcjar, srgFile, t) =>
 	
-  val infiles = new ArrayList[File]()
-  infiles.add(bin)
-  
-  val outfiles = new ArrayList[File]()
-  outfiles.add(obfzip)
-  
-  val storedInh = new ArrayList[File]()
-  storedInh.add(inh)
-    
-  val graph = CalcInheritance(null,storedInh,infiles,outfiles)
-  for( gr <- graph ) Translating(config,gr,true,infiles,outfiles)
-    
-obfzip
-}
-
-def Translating(srgFile:File, graph:MCPDInheritanceGraph, invert:Boolean, infiles:ArrayList[File],
-				    outfiles:ArrayList[File]) {
-  
-  try {
-	val excluded = new java.util.ArrayList[String]()
-    val remapper = new MCPDRemapper(srgFile,excluded,graph,invert)
+    val obfuscatedJar:File = t / (bin.base + "_ob.jar")
 	
-	val remapProcessor = new MCPDFileHandler(remapper);
-    val failures = remapProcessor.processFiles(infiles, outfiles);
-  } catch {
-    case e:IOException => //Log failure
-      
-  }
-  
+	ObfuscationHandler.TempDir = t
+	println("Calculating inheritance...")
+	val inh = ObfuscationHandler.CreateInheritanceTable(List(bin,mcjar))
+	
+	println("Reobfuscating...")
+	ObfuscationHandler.ReobfuscateJar(srgFile, inh, List(bin), List(obfuscatedJar))
+	
+	obfuscatedJar
 }
 
 val copyObfuscatedJarTask = copyObfuscated <<= (modDirectoryPath, reobfuscate) map { (modDir, modjar) =>
 	
 	val destFile:File = new File(modDir, modjar.getName()) 
-modjar #> destFile !
+	modjar #> destFile !
 }
 
 
 
 lazy val obfuscatorTasks = Seq(
-recalcInheritanceTask,
 reobfuscateTask,
 copyObfuscatedJarTask,
 retriveMCFromIvySetting
